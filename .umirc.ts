0f2b3dfd0bec6fe2e9c9a6b95c7f0ce17ecce445
yungu-2201999 <rguo1022@gmail.com>
2024-12-01 23:01:54 +0800

    umi模版搭建

---
+++ .umirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@umijs/max';
+import routes from './config/routes';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  routes,
+  npmClient: 'pnpm',
+});
